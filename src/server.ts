7f7c4ee77bce1add91c88b5f38bc470070ebf023
mdsamiulislam54 <samiulm5332@gmail.com>
2025-11-29 22:07:50 +0600

    create postgreSQL database and create pool and create user table and todos table

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,50 @@
 import express, { Request, Response } from 'express'
+import pg from 'pg'
 const app = express()
 const port = 5000
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+const pool = new pg.Pool({
+    connectionString: `postgresql://neondb_owner:npg_FDaLxqZA10vb@ep-dry-king-a4ucoz53-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+})
+
+const initDB = async () => {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            age INT NOT NULL,
+            phone VARCHAR(15),
+            address TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+        )
+        `);
+
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS todos(
+                id SERIAL PRIMARY KEY,
+                user_id INT REFERENCES users(id) ON DELETE CASCADE,
+                title VARCHAR(255) NOT NULL,
+                description TEXT,
+                completed BOOLEAN DEFAULT FALSE,
+                due_date TIMESTAMP,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `)
+}
+
+initDB().then(() => {
+    console.log('Database initialized')
+}).catch((err) => {
+    console.error('Error initializing database:', err.message)
+})
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
